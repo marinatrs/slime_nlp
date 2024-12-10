46943d185aab7843dfc326516e106058febc5199
Tibério Pereira <tiberio@fisica.ufrn.br>
2024-12-10 12:08:08 -0300

    to install: python setup.py install

---
+++ setup.py
@@ -0,0 +1,15 @@
+from stuptools import setup
+
+with open('requirements.txt', 'r') as f:
+    install_requires = f.read().splitlines()
+
+setup(name='SLIME-NLP',
+      version='0.1.0',
+      author=['Marina Ribeiro', 'Tibério Pereira'],
+      description='Statistical and Linguistic Insights for Model Explanation',
+      packages=['slime_nlp'],
+      classifiers=["Programming Language :: Python :: 3",
+                   "Operating System :: OS Independent"], 
+      #install_requires=install_requires
+)
+
