a75b53e6576dd816d463677833e173690ad99e57
Tibério Pereira <tiberio@fisica.ufrn.br>
2024-12-20 20:35:19 -0300

    add stat

--- slime_nlp/slime.py
+++ slime_nlp/slime.py
@@ -77,7 +77,7 @@ class ExplainModel:
       containing columns for tokens and associated features.
       -- features (List): list of features processed by the user-dependent tagger for visualization. Use Ellipsis (...)
       for considering an specific feature and its following ones, e.g, features=["BigWords", ...].
-      -- rand_value (int): number of random subsamples of data. 
+      -- rand_value (int): number of random subsamples of self.data. 
       -- path_results (str): string with path and dataframe results' name for saving in .csv file.
       -- return_results (bool): Boolean variable for returning the dataframe results.
     
@@ -215,7 +215,7 @@ class ExplainModel:
             display(HTML(html))
 
     
-    def attribution_by_token(self, data, path_name=None, path_plot=None, return_results=False):
+    def attribution_by_token(self, data, path_name=None, return_results=False):
     
         N = len(data)
         output = pd.DataFrame()
@@ -253,53 +253,193 @@ class ExplainModel:
         if path_name is not None:
             output.to_csv(path_name)
 
-        # NOTE: remove path_plot variable if plots are not possible here!!!!!!
-        #if path_plot is not None: 
-        #    n = path_plot.split(".")
-        #    
-        #    path_scatter = ".".join(n[:-1]) + "_scatter." + n[-1]
-        #    path_bars = ".".join(n[:-1]) + "_bars." + n[-1]
-        #    
-        #    self.plot_scatter(path_name, path_plot=path_scatter)
-        #    self.plot_bars(path_name, path_plot=path_bars)
-
         if return_results: 
             return output
 
-    
-    @staticmethod
-    def plot_scatter(path_data, path_plot=None):
-    
-        data = pd.read_csv(path_data)
+
+class Stat:
+
+    def __init__(self, path_data, features, rand_value=5000, path_results=None):
         
+        data = pd.read_csv(path_data)
+    
+        if features[-1] is Ellipsis: user_features = data.loc[:, features[0]:]
+        else: user_features = data.loc[:, features]
+        
+        data = pd.concat([data.id, data.group, data.attribution, user_features], axis=1)
+        
+        by_feature = {} 
+        Dict = {'feature':0, 'mean_attr':0, 'realp':0, 'attribution':0, 'attribute':0, 
+                'fpr':0, 'tpr':0, 'mean_attr_rand':0, 'auc_random_dist':0}
+
+        results = {'feature':[], 'AUC_impact':[], 'AUC':[], 'AUC_random':[], 'AUC_diff':[], 
+                   'group':[], 'attribution':[], 'count':[], 'percentile':[]}
+        
+        for i in user_features:
+
+            by_feature[i] = Dict
+            results['feature'].append(i)
+            
+            attribute = data.attribution.where(data[i] > 0)
+
+            by_feature[i]['attribute'] = attribute
+            by_feature[i]['attribution'] = data.attribution
+            
+            results['count'].append(np.sum(data[i]))
+            
+            mask = data[i]            
+            mean_attr_rand = [np.mean(data.attribution.where(np.random.permutation(mask) > 0)) 
+                              for _ in range(rand_value)]
+            
+            mean_attr = np.mean(data.attribution.where(mask > 0))
+
+            by_feature[i]['mean_attr_rand'] = mean_attr_rand
+            by_feature[i]['mean_attr'] = mean_attr            
+            results['attribution'].append(mean_attr)
+            
+            if (mean_attr > np.percentile(mean_attr_rand, 95)) or (mean_attr < np.percentile(mean_attr_rand, 5)):
+                if mean_attr <= 0: results['group'].append('control')
+                else: results['group'].append('condition')
+            else:
+                results['group'].append('none')
+
+            data['temp'] = data.attribution*mask
+            
+            mean = data.groupby(['id']).mean()['temp'].to_numpy()
+            median = data.groupby(['id']).median()['group'].to_numpy()
+            
+            fpr, tpr, thresholds = metrics.roc_curve(median, mean)
+            by_feature[i]['fpr'] = fpr
+            by_feature[i]['tpr'] = tpr
+            
+            realp = metrics.auc(fpr, tpr)
+            by_feature[i]['realp'] = realp
+            results['AUC'] = realp
+                        
+            auc_random_dist = np.zeros(rand_value)
+            
+            for j in range(rand_value):
+                data['temp'] = data.attribution*np.random.permutation(mask)
+                
+                mean = data.groupby(['id']).mean()['temp'].to_numpy()
+                
+                fpr, tpr, thresholds = metrics.roc_curve(median, mean)
+                
+                auc_random_dist[j] = metrics.auc(fpr, tpr)
+
+            by_feature[i]['auc_random_dist'] = auc_random_dist
+            
+            results['percentile'].append(ECDF(auc_random_dist)(realp))
+            results['AUC_random'].append(np.percentile(auc_random_dist, 50))
+            results['AUC_diff'].append(results['AUC'] - results['AUC_random'])
+            
+            if (realp > np.percentile(auc_random_dist, 95)):
+                results['AUC_impact'].append('positive')
+                
+            elif (realp < np.percentile(auc_random_dist, 5)):
+                results['AUC_impact'].append('negative')
+            else: 
+                results['AUC_impact'].append('none')
+        
+        results = pd.DataFrame(results)
+        
+        if path_results is not None:
+            results.to_csv(path_results, index = False)  
+            
+        self.by_feature = by_feature
+        self.results = results
+
+    
+    def plot_dist(self, features, path_plot=None):
+    
+        for i in features:
+            
+            fig, axs = plt.subplots(2, 2, figsize=(14, 12))
+            
+            sns.set_context(context='paper', font_scale=1.6)
+        
+            # 1st plot: Density plot of feature attribution and random distribution
+            sns.kdeplot(self.by_feature[i]['attribute'], ax=axs[0, 0], color='#d33932', label=i)
+            sns.kdeplot(self.by_feature[i]['attribution'], ax=axs[0, 0], color='black', linestyle='dashed', 
+                        label='All')
+            axs[0, 0].set_xlabel('Sample Attribution')
+            axs[0, 0].set_ylabel('Density')
+            axs[0, 0].set_frame_on(False)
+            axs[0, 0].legend(loc='upper right')   
+        
+            # 2nd plot: Histogram of average attribution of random distributions
+            axs[0, 1].hist(self.by_feature[i]['mean_attr_rand'], bins=30, color='#424243', edgecolor='white', 
+                           label='Random Permutations')
+            axs[0, 1].axvline(self.by_feature[i]['mean_attr'], color='#d33932', linewidth=3, label=i)
+            axs[0, 1].set_xlabel('Average Attribution')
+            axs[0, 1].set_ylabel('Count')
+            axs[0, 1].set_frame_on(False)
+            axs[0, 1].legend(loc='best')  
+            
+            # 3rd plot: ROC curve of condition vs control
+            axs[1, 0].plot(self.by_feature[i]['fpr'], self.by_feature[i]['tpr'], color='#d33932', lw=3, 
+                           label=f'Control vs Condition (AUC = {self.by_feature[i]['realp']:.2f})')
+            axs[1, 0].plot([0, 1], [0, 1], 'k--', label='Chance level (AUC = 0.5)')  # Diagonal line
+            axs[1, 0].set_xlabel('False Positive Rate')
+            axs[1, 0].set_ylabel('True Positive Rate')
+            axs[1, 0].fill_between(self.by_feature[i]['fpr'], self.by_feature[i]['tpr'], facecolor='#d33932', 
+                                   alpha=.3)
+            axs[1, 0].set_frame_on(False)
+            axs[1, 0].legend(loc='lower right')
+             
+            # 4th plot: Histogram of AUC values of random distributions
+            axs[1, 1].hist(self.by_feature[i]['auc_random_dist'], bins=100, color='#424243', edgecolor='white', 
+                           label='Random permutations')
+            axs[1, 1].axvline(self.by_feature[i]['realp'], color='#d33932', lw=3, label=i)
+            axs[1, 1].axvline(np.percentile(self.by_feature[i]['auc_random_dist'], 50), color='#efe74e', lw=3, 
+                              label='(median)')
+            axs[1, 1].set_xlabel('AUC')
+            axs[1, 1].set_ylabel('Count')
+            axs[1, 1].set_frame_on(False)
+            axs[1, 1].legend(loc='best')    
+            
+            # Final adjustments
+            plt.tight_layout()
+            plt.rcParams['pdf.fonttype'] = 42 
+        
+            if path_plot is not None: 
+                n = path_plot.split(".")
+                path_plot = ".".join(n[:-1]) + f"_{i}." + n[-1]
+                plt.savefig(path_plot)     
+            else: 
+                plt.show()
+
+
+    def plot_scatter(self, path_plot=None):
+    
         plt.figure(figsize=(14, 9), facecolor='white')
         sns.set_theme(context='talk', style='white')
         
-        markers = {"control": "o", "none": "s", "AD": "X"}
+        markers = {"control": "o", "none": "s", "condition": "X"}
         custom_palette = {'positive': 'green', 'negative': 'gray', 'none': 'gray'}
         
-        sns.scatterplot(data=data, x='attribution', y='AUC_diff', hue='AUC_impact', style="group",
+        sns.scatterplot(data=self.results, x='attribution', y='AUC_diff', hue='AUC_impact', style="group",
                         markers=markers, palette=custom_palette, s=200)
         
         plt.axhline(0, color='gray', linestyle='--', linewidth=1)
         plt.axvline(0, color='gray', linestyle='--', linewidth=1)
         
-        plt.annotate('Control', xy=(data['attribution'].min(), 0), xycoords='data', 
+        plt.annotate('Control', xy=(self.results['attribution'].min(), 0), xycoords='data', 
                      xytext=(-70, -5), textcoords='offset points', ha='center', color='gray')
         
-        plt.annotate('Condition', xy=(data['attribution'].max(), 0), xycoords='data', 
+        plt.annotate('Condition', xy=(self.results['attribution'].max(), 0), xycoords='data', 
                      xytext=(80, -5), textcoords='offset points', ha='center', color='gray')
         
-        plt.annotate('Help classification', xy=(0, data['AUC_diff'].max()), xycoords='data', 
+        plt.annotate('Help classification', xy=(0, self.results['AUC_diff'].max()), xycoords='data', 
                      xytext=(5, 40), textcoords='offset points', ha='center', color='gray')
         
-        plt.annotate('Worsen classification', xy=(0, data['AUC_diff'].min()), xycoords='data', 
+        plt.annotate('Worsen classification', xy=(0, self.results['AUC_diff'].min()), xycoords='data', 
                      xytext=(5, -40), textcoords='offset points', ha='center', color='gray')
         
-        labels = data.feature
-        for i in range(len(data.feature)):
-            if data.AUC_impact[i] == 'positive':
-                plt.annotate(labels[i], (data.attribution[i], data.AUC_diff[i]), 
+        labels = self.results.feature
+        for i in range(len(self.results.feature)):
+            if self.results.AUC_impact[i] == 'positive':
+                plt.annotate(labels[i], (self.results.attribution[i], self.results.AUC_diff[i]), 
                              textcoords="offset points", xytext=(0,10), ha='center')
         
         plt.xlabel('Attribution')
@@ -320,17 +460,14 @@ class ExplainModel:
         
         if path_plot is not None: plt.savefig(path_plot, dpi=300)
         else: plt.show()
-    
 
-    @staticmethod
-    def plot_bars(path_data, path_plot=None):
     
-        data = pd.read_csv(path_data)
-            
+    def plot_bars(self, path_plot=None):
+    
         sns.set_theme(context='talk', style='white') 
         
         def get_color(row):
-            base_color = {'AD': '#e2a8a7', 'none': '#7b7b7b', 'control': '#73949e'}
+            base_color = {'condition': '#e2a8a7', 'none': '#7b7b7b', 'control': '#73949e'}
             alpha = 1.0 if row['AUC_impact'] == 'positive' else 0.7
             return base_color[row['group']], alpha
     
@@ -341,7 +478,7 @@ class ExplainModel:
         
         fig, ax = plt.subplots(1, 2, figsize=(10,12)) 
         
-        filtered_data = data[(data['attribution'] != 0) & (~data['attribution'].isna())]
+        filtered_data = self.results[(self.results['attribution'] != 0) & (~self.results['attribution'].isna())]
         N = len(filtered_data)
         
         for i in [0, 1]:
@@ -381,132 +518,3 @@ class ExplainModel:
                 
         if path_plot is not None: plt.savefig(path_plot, dpi=300)
         else: plt.show()
-
-
-    @staticmethod
-    def stat(path_data, features, rand_value=5000, path_results=None, path_plot=None, return_results=False):
-        
-        data = pd.read_csv(path_data)
-        
-        if features[-1] is Ellipsis: user_features = data.loc[:, features[0]:]
-        else: user_features = data.loc[:, features]
-        
-        data = pd.concat([data.id, data.group, data.attribution, user_features], axis=1)
-    
-        feature, AUC_impact, AUC, group, attr, count, percentile, AUC_random = [], [], [], [], [], [], [], []
-        
-        for i in user_features:
-            
-            feature.append(i)
-            
-            attribute = data.attribution.where(data[i] > 0)
-            
-            count.append(np.sum(data[i]))
-        
-            mask = data[i]
-            
-            mean_attr_rand = [np.mean(data.attribution.where(np.random.permutation(mask) > 0)) 
-                              for _ in range(rand_value)]
-            
-            mean_attr = np.mean(data.attribution.where(mask > 0))
-            
-            attr.append(mean_attr)
-            
-            if (mean_attr > np.percentile(mean_attr_rand, 95)) or (mean_attr < np.percentile(mean_attr_rand, 5)):
-                if mean_attr <= 0: group.append('control')
-                else: group.append('condition')    
-            else:
-                group.append('none')
-        
-            data['temp'] = data.attribution*mask
-            
-            mean = data.groupby(['id']).mean()['temp'].to_numpy()
-            median = data.groupby(['id']).median()['group'].to_numpy()
-            
-            fpr, tpr, thresholds = metrics.roc_curve(median, mean)
-            
-            realp = metrics.auc(fpr, tpr)
-    
-            AUC.append(realp)
-            
-            auc_random_dist = np.zeros(rand_value)
-            
-            for j in range(rand_value):
-                data['temp'] = data.attribution*np.random.permutation(mask)
-                
-                mean = data.groupby(['id']).mean()['temp'].to_numpy()
-                
-                fpr, tpr, thresholds = metrics.roc_curve(median, mean)
-                
-                auc_random_dist[j] = metrics.auc(fpr, tpr)
-            
-            AUC_random.append(np.percentile(auc_random_dist, 50))
-            percentile.append(ECDF(auc_random_dist)(realp))
-            
-            if (realp > np.percentile(auc_random_dist, 95)):
-                AUC_impact.append('positive')
-            elif (realp < np.percentile(auc_random_dist, 5)):
-                AUC_impact.append('negative')
-            else: 
-                AUC_impact.append('none')
-      
-            fig, axs = plt.subplots(2, 2, figsize=(14, 12))
-            
-            sns.set_context(context='paper', font_scale=1.6)
-    
-            # 1st plot: Density plot of feature attribution and random distribution
-            sns.kdeplot(attribute, ax=axs[0, 0], color='#d33932', label=i)
-            sns.kdeplot(data.attribution, ax=axs[0, 0], color='black', linestyle='dashed', label='All')
-            axs[0, 0].set_xlabel('Sample Attribution')
-            axs[0, 0].set_ylabel('Density')
-            axs[0, 0].set_frame_on(False)
-            axs[0, 0].legend(loc='upper right')   
-    
-            # 2nd plot: Histogram of average attribution of random distributions
-            axs[0, 1].hist(mean_attr_rand, bins=30, color='#424243', edgecolor='white', label='Random Permutations')
-            axs[0, 1].axvline(mean_attr, color='#d33932', linewidth=3, label=i)
-            axs[0, 1].set_xlabel('Average Attribution')
-            axs[0, 1].set_ylabel('Count')
-            axs[0, 1].set_frame_on(False)
-            axs[0, 1].legend(loc='best')  
-            
-            # 3rd plot: ROC curve of condition vs control
-            axs[1, 0].plot(fpr, tpr, color='#d33932', lw=3, label=f'Control vs Condition (AUC = {realp:.2f})')
-            axs[1, 0].plot([0, 1], [0, 1], 'k--', label='Chance level (AUC = 0.5)')  # Diagonal line
-            axs[1, 0].set_xlabel('False Positive Rate')
-            axs[1, 0].set_ylabel('True Positive Rate')
-            axs[1, 0].fill_between(fpr, tpr, facecolor='#d33932', alpha=.3)
-            axs[1, 0].set_frame_on(False)
-            axs[1, 0].legend(loc='lower right')
-             
-            # 4th plot: Histogram of AUC values of random distributions
-            axs[1, 1].hist(auc_random_dist, bins=100, color='#424243', edgecolor='white', label='Random permutations')
-            axs[1, 1].axvline(realp, color='#d33932', lw=3, label=i)
-            axs[1, 1].axvline(np.percentile(auc_random_dist, 50), color='#efe74e', lw=3, label='(median)')
-            axs[1, 1].set_xlabel('AUC')
-            axs[1, 1].set_ylabel('Count')
-            axs[1, 1].set_frame_on(False)
-            axs[1, 1].legend(loc='best')    
-            
-            # Final adjustments
-            plt.tight_layout()
-            plt.rcParams['pdf.fonttype'] = 42 
-
-            if path_plot is not None: 
-                n = path_plot.split(".")
-                path_plot = ".".join(n[:-1]) + f"_{i}." + n[-1]
-                
-                plt.savefig(path_plot)
-                
-            else: 
-                plt.show()
-            
-        df_results = pd.DataFrame({'feature': feature, 'AUC_impact': AUC_impact, 'AUC': AUC, 
-                                   'AUC_random': AUC_random, 'percentile': percentile, 
-                                   'count': count, 'group': group, 'attribution': attr})
-    
-        if path_results is not None:
-            df_results.to_csv(path_results, index = False)
-            
-        if return_results: return df_results
-
