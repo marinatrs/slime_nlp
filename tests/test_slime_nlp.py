e7f6d684cf18c07a2abd70330a10e72264682baf
Tibério Pereira <tiberio@fisica.ufrn.br>
2024-12-21 14:09:32 -0300

    test

--- tests/test_slime_nlp.py
+++ tests/test_slime_nlp.py
@@ -4,11 +4,11 @@ from slime_nlp import __version__
 from slime_nlp.dataset import ImportData
 from slime_nlp.model import CustomModel
 from slime_nlp.slime import ExplainModel
+from slime_nlp.slime import Stat
 
 def test_version():
     assert __version__ == '0.1.0'
     
-
 def test_slime():
     
     # test dataframe import:
@@ -43,7 +43,12 @@ def test_slime():
     """ExplainModel().attribution_by_token() should return a dataframe with "condition", "group", "pred_label", "score", "attributions", and "token" columns!"""
 
     # test visualization explanability:
-    exp.visualize(df, path_name="test_results.png")
+    ###exp.visualize(df, path_name="test_results.png")
     
     assert os.path.exists('test_results.png'), \
     """ExplainModel().visualize() should save the visualization results into test_results.png!"""
+
+    # test Stat:
+    stat = Stat(path_data=os.getcwd() + "/tests/interp_test.csv", features=["feature"], rand_value=10)
+    assert str(type(stat.results)) == "<class 'pandas.core.frame.DataFrame'>", \
+    """Stat() initialization should create a "results" dataframe!"""
